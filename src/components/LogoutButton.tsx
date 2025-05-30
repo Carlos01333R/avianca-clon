ac360c16784ca02112343b6217ce50a5c3451a5c
Carlos01333R <carlosrodriguezanavila0@gmail.com>
2025-05-30 14:42:54 -0500

    arreglar la ruta en el logout

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -8,7 +8,7 @@ export default function LogoutButton() {
 
   const handleLogout = async () => {
     await supabase.auth.signOut()
-    router.push('/login')
+    router.push('/loginRegistros')
   }
 
   return (
